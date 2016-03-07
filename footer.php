a1e8fe84215b402613e8f5c678183f4ed336b192
purenine <thomasklinger@gmail.com>
2016-03-07 10:38:14 -0500

    new delete

--- footer.php
+++
@@ -1,9 +0,0 @@
-	
-		</div><!--/#site-wrap-->
-	
-	</body>
-	
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-	<script src="assets/js/p9.js"></script>
-	
-</html>
