8ba7cb0b15f5f41e0e6ff16acde267ca118b6dfa
purenine <thomasklinger@gmail.com>
2016-03-07 11:16:51 -0500

    meta data

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<head>
 	    <meta charset="utf-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
-	    <title>PureNine Studios</title>
+	    <title>PURENINE | A digital creative studio owned and operated by Thomas Klinger</title>
 	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	    <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,700' rel='stylesheet' type='text/css'>
 	    <link href='https://fonts.googleapis.com/css?family=Raleway:400,500,700' rel='stylesheet' type='text/css'>
