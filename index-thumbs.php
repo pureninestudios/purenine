c305e56d2ab75f02f48e0f0379dedf3e7bd299ba
Dean Elliott <dean.elliott1985@live.co.uk>
2016-04-11 09:58:58 +0100

    New thumb layout

---
+++ index-thumbs.php
@@ -0,0 +1,556 @@
+<?php include ('header.php'); ?>
+
+<body class="home-1">
+
+<!-- Pre Loader -->
+<div class="pre-loader"></div>
+<div class="pre-loader-anim">
+	<span>P&nbsp;&nbsp;9</span>
+</div>
+<!-- /Pre Loader -->	
+	 
+<div class="wrapper">
+	
+	<!-- Landing Content -->
+	<a class="goto-work" href="#work">
+		<div id="hero" class="parallax-hero img-wraper full-height">
+			<div class="img-onload-wrap"></div>
+			<div class="text-wrap">
+				<div class="text-wrap-content">
+					<span class="head-text">We are a digital studio offering solutions <br> in product development, mobile <br> and visual design.</span>
+					<span class="sep"></span>
+					<div class="head-detail">
+						<span>See our Work</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</a>
+	<!--/ Landing Content -->
+	
+	<!--Main Content -->
+	<div id="work" class="main-content">
+
+<?php include ('nav.php'); ?>
+			
+<!--Main Content -->	
+<div class="wrapper ">
+	
+	<div id="work" class="main-content">
+		<div id="content" class="padding-sec-lg">			
+			<ul id="portfolio-wrap" class="clearfix">
+
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="madden.php">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-madden.jpg">
+								
+								<div class="faux-bg" style="background-color:#422673"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">GoodGame Agency</p>
+									
+									<h3>Madden Live Challenge</h3>
+									
+									<p class="tags">Art Direction, UX, UI, Visual Design</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+								
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="http://www.pgatour.com">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-pga.jpg">
+								
+								<div class="faux-bg" style="background-color:#021E36"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Omnigon</p>
+									
+									<h3>PGATour.com</h3>
+									
+									<p class="tags">UX, Visual Design, Mobile</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+				
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="http://www.proactiv.com/how-does-proactiv-work.html">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-proactiv.jpg">
+								
+								<div class="faux-bg" style="background-color:#54A1A9"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Haven Agency</p>
+									
+									<h3>Proactiv.com</h3>
+									
+									<p class="tags">Art Direction, UX, Visual Design</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+				
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="http://www.guardianlife.com">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-guardian.jpg">
+								
+								<div class="faux-bg" style="background-color:#124074"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Guardianlife</p>
+									
+									<h3>GuardianLife.com</h3>
+									
+									<p class="tags">Art Direction, Visual Design, Styleguide</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+				
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="http://www.audiomack.com">
+							
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-am.jpg">
+								
+								<div class="faux-bg" style="background-color:#323232"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Audiomack</p>
+									
+									<h3>Audiomack.com</h3>
+									
+									<p class="tags">Creative Direction, UX, UI, Visual Design, Mobile</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+				
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="activision.php">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-activision.jpg">
+								
+								<div class="faux-bg" style="background-color:#353B4B"></div>
+							
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Haven Agency</p>
+									
+									<h3>Shop Activision.com</h3>
+									
+									<p class="tags">Visual Design, UX, E-commerce</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+				
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="pennzoil.php">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-pennzoil.jpg">
+								
+								<div class="faux-bg" style="background-color:#303030"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Haven Agency</p>
+									
+									<h3>Pennzoil Sweepstakes</h3>
+									
+									<p class="tags">Concept, UX, Visual Design, Social</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+				
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="1d.php">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-1d.jpg">
+								
+								<div class="faux-bg" style="background-color:#8F5EA0"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Livenation</p>
+									
+									<h3>One Direction Tour</h3>
+									
+									<p class="tags">UX, UI, Visual Design</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+				
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="highlights.php">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-highlights.jpg">
+								
+								<div class="faux-bg" style="background-color:#415266"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Highlights</p>
+									
+									<h3>Highlights.com</h3>
+									
+									<p class="tags">Pitch, Visual Design, UX, UI, Mobile</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+						
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="bio.php">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-bio.jpg">
+								
+								<div class="faux-bg" style="background-color:#024D77"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">A+E Networks</p>
+									
+									<h3>Biography.com</h3>
+									
+									<p class="tags">Creative Direction, UX, UI, Visual Design</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="indycar.php">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-indy.jpg">
+								
+								<div class="faux-bg" style="background-color:#CA1B18"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Omnigon</p>
+									
+									<h3>Verizon IndyCar</h3>
+									
+									<p class="tags">Visual Design, Mobile, Tablet</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+				
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="toyota.php">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-toyota.jpg">
+								
+								<div class="faux-bg" style="background-color:#666666"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">People.com</p>
+									
+									<h3>Toyota Avalon</h3>
+									
+									<p class="tags">Advertising, UX, UI, Visual Design, Tablet</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+				
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="jt.php">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-jt.jpg">
+								
+								<div class="faux-bg" style="background-color:#232323"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Livenation</p>
+									
+									<h3>Legends of Summer Tour </h3>
+									
+									<p class="tags">Creative Direction, UX, UI, Visual Design</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="mortalkombat.php">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-mk.jpg">
+								
+								<div class="faux-bg" style="background-color:#301011"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Haven Agency</p>
+									
+									<h3>Mortal Kombat</h3>
+									
+									<p class="tags">Pitch, Concept, UX, UI, Visual Design</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+
+				<li class="item-wrap">
+				
+					<div class="grid-item">
+					
+						<a href="thq.php">
+						
+							<div class="work-image">
+						
+								<img src="https://s3-us-west-2.amazonaws.com/purenine/hp-thq.jpg">
+								
+								<div class="faux-bg" style="background-color:#442956"></div>
+								
+							</div><!--/.work-image-->
+							
+							<div class="overlay-block">
+								
+								<div class="overlay-content">
+							
+									<p class="name">Haven Agency</p>
+									
+									<h3>THQ.com</h3>
+									
+									<p class="tags">Pitch, Concept, Visual Design</p>
+								
+								</div><!--/.overlay-content-->
+								
+							</div><!--/.overlay-->
+	
+						</a>
+						
+					</div><!--/.grid-item-->
+				
+				</li><!--/.item-wrap-->
+	
+			</ul>
+		</div>
+		
+<?php include ('footer.php'); ?>
