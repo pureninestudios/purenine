959cab068e56a6b48663a04299e5e36ba575ff21
Thomas Klinger <thomasklinger@gmail.com>
2016-04-18 14:21:05 -0400

    updated thq

--- thq.php
+++ thq.php
@@ -1,93 +1,41 @@
 <?php include ('header.php'); ?>
 
-
-<body class="project-detail-page-type-2">
+<body class="singleblog-page">
 
 <?php include ('loader.php'); ?>
 
-<!-- Header Sec -->
 <?php include ('nav.php'); ?>
 
-<!--Main Content -->
-<div class="wrapper ">
-	<div class="content-without-footer">
-		<section class="padding-sec-top-xl">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-7 align-center text-center">
-						<h1>THQ.com Redesign</h1>
-						<span class="big-sep">~</span>
-					</div>
-				</div>	
-			</div>
-			<div  id="galpost_owl" class="owl-carousel owl-theme zoomslider">
-				<div class="item zoomslider-item">
-					<img class="get-color" src="images/gallery/scrollsliderimg1.jpg" data-width="1170" data-height="650" alt="horiimage">
-					<a  href="images/gallery/scrollsliderimg1.jpg">
-						<div class="expandtozoom">
-							<img class="fa-expand" src="images/expand.svg" alt="icon" />
-						</div>
-					</a>
-				</div>
-				<div class="item zoomslider-item">
-					<img class="get-color" src="images/gallery/scrollsliderimg2.jpg" data-width="1170" data-height="650" alt="horiimage">
-					<a  href="images/gallery/scrollsliderimg2.jpg" >
-						<div class="expandtozoom">
-							<img class="fa-expand" src="images/expand.svg" alt="icon" />
-						</div>
-					</a>	
-					
-				</div>
-				<div class="item zoomslider-item">
-					<img class="get-color" src="images/gallery/scrollsliderimg3.jpg" data-width="1170" data-height="650" alt="horiimage">
-					<a href="images/gallery/scrollsliderimg3.jpg">
-						<div class="expandtozoom">
-							<img class="fa-expand" src="images/expand.svg" alt="icon" />
-						</div>
-					</a>
-					
-				</div>
-				<div class="item zoomslider-item">
-					<img class="get-color" src="images/gallery/scrollsliderimg4.jpg" data-width="512" data-height="650" alt="horiimage">
-					<a  href="images/gallery/scrollsliderimg4.jpg" >
-						<div class="expandtozoom">
-							<img class="fa-expand" src="images/expand.svg" alt="icon" />
-						</div>
-					</a>
+<div class="wrapper">
+	<section class="container padding-sec-top-xl">
+		<div class="row">
+			<div class="col-sm-8 align-center text-center margin-bottom-xs-50">
+				<h1>THQ.com Redesign</h1>
+				<div class="blog-dtails">
+					<ul>
+						<li>Haven Agency</li>
+					</ul>
 				</div>
 			</div>
-		</section>
-		
-		<section class="container padding-sec-top-sm">
-			<div class="row">
-				<div class="col-sm-8 align-center text-left  clearfix detail-dis">
-					<div class="row">
-						<div class="col-lg-12">
-							<h6>client</h6>
-							<span>Fusion Models NYC</span>
-						</div>
-						<div class="col-lg-12">
-							<h6>photographer</h6>
-							<span>Andrew Willson</span>
-						</div>
-						<div class="col-lg-12">
-							<h6>camera</h6>
-							<span>CANON EOS 6D</span>
-						</div>
-					</div>
-					<div class="clearfix"></div>
-					<hr/>
-					<p class="lead padding-sec-bottom-sm">Many people think that typography consists of only selecting a typeface, choosing a font size and whether it should be regular or bold.</p>
-					<p class="padding-sec-bottom-sm">New fine art prints,Order one or few prints to make diptych or triptych compositions. Tiny women carry amazingly large bags, twice their eighty-pound weight. Some of the women carry babies in around front. </p>
-					
+			
+			<img class="col-sm-12" src="images/blog/image1.jpg" alt="blog" />
+			
+			<div class="col-sm-8 align-center text-left clearfix padding-sec-top-sm">
+				<p class="lead padding-sec-bottom-sm">An adventurous life does not necessarily mean climbing mountains, swimming with sharks, or jumping off cliffs. It means risking yourself by leaving a little piece of you behind in all those you meet along the way.</p>
+				<p class="padding-sec-bottom-sm">New fine art prints, <b>Darkness, available in the Shop.</b> Order one or few prints to make diptych or triptych compositions. Tiny women carry amazingly large bags, twice their eighty-pound weight. Some of the women carry babies in <u><i>slings</i></u> around front. </p>
+				
+				<div class="blogger-img zoomslider padding-sec-bottom-sm">
+					<div class="zoomslider-item single-image">
+						<a  href="images/blog/image2.jpg">
+							<img src="images/blog/image2.jpg" alt="" />
+							<img class="magf" src="images/icon4.png"  alt="icon"/>
+						</a>
+					</div>	
 				</div>
 			</div>
-		</section>
-		
-		
-
-		
-	</div>
+		</div>
+	</section>
+	
 </div>
 <!--/Main Content -->
 
