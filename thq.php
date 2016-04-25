9f9c318f57eabe45c4bbcb8125a616f6583abc25
Dean Elliott <dean.elliott1985@live.co.uk>
2016-04-25 09:33:38 +0100

    New project top

--- thq.php
+++ thq.php
@@ -6,6 +6,64 @@
 
 <?php include ('nav.php'); ?>
 
+	<div class="wrapper">
+	
+		<div class="container padding-sec-top-xl project-inner">
+	
+			<div class="row">
+				
+				<div class="project-info col-xs-12 col-md-8 col-lg-9">
+				
+					<h1 class="project-title">THQ.com</h1>
+					
+					<div class="project-copy">
+					
+						<p>We were asked to help pitch some designs for an all new THQ.com. We wanted the experience to showcase not only the most popular games but enhance some of the smaller titles. We fit the layout into a responsive layout to allow for all platforms. A custom search feature allowed users to see multiple games for a quick and easy experience. Deliverables included a homepage and interior shop page with responsive breakpoints.</p>
+						
+					</div><!--/.project-copy-->
+				
+				</div><!--/.project-info-->
+				
+				<div class="client-info col-xs-12 col-md-4 col-lg-3">
+				
+					<ul>
+					
+						<li>
+						
+							<span>Client</span>
+							
+							THQ Games
+						
+						</li>
+						
+						<li>
+						
+							<span>Services</span>
+							
+							UX, UI, Visual Design, Pitch
+						
+						</li>
+						
+						<li>
+						
+							<span>Agency</span>
+							
+							Haven Agency
+						
+						</li>
+					
+					</ul>
+				
+				</div><!--/.client-info-->
+			
+			</div><!--/.row-->
+		
+		</div><!--/.container-->
+	
+	</div><!--/.wrapper-->
+
+<?php /* ORIGINAL
+
 <div class="wrapper">
 	<section class="container padding-sec-top-xl">
 		<div class="row">
@@ -39,5 +97,7 @@
 	
 </div>
 <!--/Main Content -->
+
+*/ ?>
 
 <?php include ('footer.php'); ?>
