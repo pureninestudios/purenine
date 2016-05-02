10c64f753975bd4e719c35ae8e1e5be7d2f355e3
purenine <thomasklinger@gmail.com>
2016-05-01 21:50:50 -0400

    sad

---
+++ loader.php
@@ -0,0 +1,4 @@
+<div class="pre-loader"></div>
+<div class="pre-loader-anim">
+	<span>P&nbsp;&nbsp;9</span>
+</div>
---
+++ thq.php
@@ -0,0 +1,141 @@
+<?php include ('header.php'); ?>
+<body class="project-detail-page-type-1">
+
+<!-- Pre Loader -->
+<?php include ('loader.php'); ?>
+<!-- /Pre Loader -->	
+		
+<!-- Header Sec -->
+
+<?php include ('nav.php'); ?>
+
+<!--Main Content -->		
+<div class="wrapper full-height wrapper-anim">
+		<div class="nav-arrow-wrap">
+			<a href="#" id="arrow_left">
+				<span class="arrow-animation top">
+					<img src="images/uparrow-light.svg" alt="arrow" />
+				</span>
+			</a>
+			<span class="anim-line">
+				<span class="left-line"></span>
+				<span class="right-line"></span>
+			</span>
+			<a href="#" id="arrow_right">
+				<span class="arrow-animation bottom">
+					<img src="images/downarrow-light.svg" alt="arrow" />
+				</span>
+			</a>
+		</div>
+	<div class="resizable">
+		<img class="fa-expand" src="images/expand.svg" alt="icon" />
+		<img class="fa-compress opac-hide" src="images/contract.svg" alt="icon" />
+	</div>
+	<div id="maximage">
+		<img src="imgs/port-sm-thq-1.jpg" alt="" />
+		<img src="imgs/port-sm-thq-2.jpg" alt="" />
+		<img src="imgs/port-sm-thq-3.jpg" alt="" />
+	</div>
+</div>
+<div class="project-detail-content text-center">
+	<div class="resizable-detail">
+		<img class="fa-expand" src="images/expand-dark.svg" alt="icon" />
+		<img class="fa-compress opac-hide" src="images/contract-dark.svg" alt="icon" />
+	</div>
+	<div class="short-dis">
+		<div class="container-fluid">
+			<h4>summer photowalk 2016</h4>
+			<ul>
+				<li>12.01.16</li>
+				<li>~</li>
+				<li>Photography</li>
+			</ul>
+		</div>
+	</div>
+	
+	
+	
+	<div class="detail-dis">
+		<div class="container-fluid">
+			<h3>THQ.com Redesign</h3>
+			<div class="row  margin-top-xs-50">
+				<div class="col-xs-2">
+					<h6>client</h6>
+					<span>THQ</span>
+				</div>
+				<div class="col-xs-2">
+					<h6>Agency</h6>
+					<span>Haven Agency</span>
+				</div>
+				<div class="col-xs-2">
+					<h6>Services</h6>
+					<span>Art Direction, UX, UI, Visual Design</span>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<hr/>
+			<p class="padding-sec-top-xs col-lg-10 align-center">Many people think that typography consists of only selecting a typeface, choosing a font size and whether it should be regular or bold. For most people it ends there. But there is much more to achieving good typography and it's in the details that designers often neglect.</p>
+			<ul class="social-menu">
+				<li>share</li>
+				<li>
+					<ul>
+						<li><a href=""><i class="fa fa-facebook-square"></i></a></li>
+						<li><a href=""><i class="fa fa-twitter"></i></a></li>
+						<li><a href=""><i class="fa fa-instagram"></i></a></li>
+					</ul>	
+				</li>
+			</ul>
+		</div>
+		
+		
+	</div>
+	<!--nav to projects-->
+		<ul class="proj-nav">
+			<li class="prev-page">
+				<a href="projectdetail2.html">
+					<span><img src="images/leftarrow-dark.svg" alt="left" /></span>
+				</a>	
+			</li>
+			<li class="archve">
+				<a class="goto-partfolio" href="horizontalgalleryportfolio.html">
+					<div>
+						<span>
+							<span></span><span></span>
+						</span>	
+						<span>
+							<span></span><span></span>
+						</span>	
+						<span>	
+							<span></span><span></span>
+						</span>	
+					</div>	
+				</a>
+			</li>
+			<li class="next-page">
+				<a href="projectdetail2.html">
+					<span><img src="images/rightarrow-dark.svg" alt="right" /></span>
+				</a>	
+			</li>
+		</ul>
+	<!--/nav to projects-->
+				
+</div>
+<!--Main Content -->		
+
+<!-- JQuery -->		
+<script type="text/javascript" src="js/jquery-1.11.2.js"></script>
+<script type="text/javascript" src="js/jquery.easings.min.js"></script>
+<script type="text/javascript" src="js/modernizr.custom.js"></script>
+<script type='text/javascript' src="js/bootstrap.min.js"></script>
+<script type='text/javascript' src="js/jquery.nicescroll.js"></script>
+<script type="text/javascript" src="js/jquery.cycle.all.min.js"></script>
+<script type="text/javascript" src="js/jquery.maximage.min.js" ></script>
+<script type='text/javascript' src="js/overlay-menu.js"></script>
+<script type="text/javascript" src="js/jquery.multiscroll.js"></script>
+<script type="text/javascript" src="js/jquery.primarycolor.js"></script>
+<script type='text/javascript' src="js/jquery.vimeo.api.js"></script>
+<script type='text/javascript' src="js/jquery.colorbox.js"></script>
+<script type='text/javascript' src="js/scripts.js"></script>
+		
+</body>
+</html>
