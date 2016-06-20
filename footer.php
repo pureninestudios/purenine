c9d48100c70d339cc479972f40dee2e5bed11cdb
Dean Elliott <dean.elliott1985@live.co.uk>
2016-06-20 16:43:12 +0100

    Copyright symbol

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
 		                </div>
 
 		                <div class="column one-half last-col align-right">
-		                 	<small>© 2016 PURENINE Studios Inc.</small>
+		                 	<small>&copy; 2016 PURENINE Studios Inc.</small>
 		                </div>
 		            </div>
 		        </div>
