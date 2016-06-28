bc70d675e700b27b6132573f86e7e6496a432355
purenine <thomasklinger@gmail.com>
2016-06-28 15:45:01 -0400

    New studio stuff

--- studio.php
+++ studio.php
@@ -1,24 +1,23 @@
 <?php include('header.php'); ?>
 
-	<div class="fullwidth-section text-light parallax-section" data-parallax-image="files/uploads/1680x1100-dark.jpg">
+	<div class="fullwidth-section text-light parallax-section" data-parallax-image="imgs/hero-1600x1100.jpg">
     	<div class="fullwidth-content wrapper">
            	<div class="spacer-medium"></div>
           	<div class="column-section clearfix">
             	<div class="column one-half last-col align-center">
-            		<p><img src="files/uploads/est.png" srcset="files/uploads/est.png 1x, files/uploads/est@2x.png 2x" alt="Established"></p>
                     <div class="spacer-medium"></div>
-                	<h6 class="title-alt uppercase">Where the magic happens</h6>
-                    <h2>We are a <strong><em>digital studio</em></strong> based in Sydney, crafting digital experiences.</h2>
+                	<!--<h6 class="title-alt uppercase">Experience Creativity</h6>-->
+                    <h2>We are a <em>digital design studio</em> started back in 2009.</h2>
                 </div>
             </div>
            	<div class="spacer-medium"></div>
         </div>
-    </div> <!-- END .fullwidth-section -->
+    </div>  <!--END .fullwidth-section -->
 
 	<div class="spacer-big"></div>
 
     <div class="wrapper-small">
-        <h2>Distinctive and striking digital products</h2>
+        <h2>We offer solutions in product thinking, visual design and mobile.</h2>
         <div class="column-section clearfix">
             <div class="column one-half">
                 <p>Possession her thoroughly remarkably terminated man continuing. Removed greater to do ability. You shy shall while but wrote marry. Call why sake has sing pure. Gay six set polite nature worthy. So matter be me we wisdom should basket moment merely. Me burst ample wrong which would mr he could.</p>
@@ -34,49 +33,13 @@
     <div class="wrapper">
         <div class="column-section clearfix">
             <div class="column one-third team-member">
-            	<div class="thumb-hover overlay-effect text-dark">
-                    <img src="files/uploads/600x600-light.jpg" alt="SEO">
-                    <div class="overlay-caption hidden bottom align-left">
-                        <span class="caption-sub team-role">Creative Director</span>
-                        <h3 class="caption-name team-name"><strong>Tom Burly</strong></h3>
-                    </div>
-                    <ul class="socialmedia-widget">
-                        <li class="facebook"><a href="#"></a></li>
-                        <li class="twitter"><a href="#"></a></li>
-                        <li class="instagram"><a href="#"></a></li>
-                        <li class="mail"><a href="#"></a></li>
-                    </ul>
-                </div>
+                <img src="imgs/box-600x600-1.jpg" alt="">  
             </div>
             <div class="column one-third team-member">
-            	<div class="thumb-hover overlay-effect text-dark">
-                    <img src="files/uploads/600x600-light.jpg" alt="SEO">
-                    <div class="overlay-caption hidden bottom align-left">
-                        <span class="caption-sub team-role">Senior Designer</span>
-                        <h3 class="caption-name team-name"><strong>Alice Boga</strong></h3>
-                    </div>
-                    <ul class="socialmedia-widget">
-                        <li class="behance"><a href="#"></a></li>
-                        <li class="twitter"><a href="#"></a></li>
-                        <li class="googleplus"><a href="#"></a></li>
-                        <li class="mail"><a href="#"></a></li>
-                    </ul>
-                </div>
+                <img src="imgs/box-600x600-1.jpg" alt="">
             </div>
             <div class="column one-third last-col team-member">
-            	<div class="thumb-hover overlay-effect text-dark">
-                    <img src="files/uploads/600x600-light.jpg" alt="SEO">
-                    <div class="overlay-caption hidden bottom align-left">
-                        <span class="caption-sub team-role">Web Designer</span>
-                        <h3 class="caption-name team-name"><strong>Jhon White</strong></h3>
-                    </div>
-                    <ul class="socialmedia-widget">
-                        <li class="dribbble"><a href="#"></a></li>
-                        <li class="vimeo"><a href="#"></a></li>
-                        <li class="mail"><a href="#"></a></li>
-                        <li class="url"><a href="#"></a></li>
-                    </ul>
-                </div>
+                <img src="imgs/box-600x600-1.jpg" alt="">
             </div>
         </div>
     </div> <!-- END .wrapper -->
@@ -87,7 +50,7 @@
         <h6 class="title-alt uppercase">We enjoy meeting new people</h6>
         <h2>Interested in working with us?</h2>
         <div class="spacer-medium"></div>
-        <p><a href="page-contact.html" class="sr-button button-big">Contact us</a></p>
+        <p><a href="mailto:hello@purenine.com" class="sr-button button-big">Contact us</a></p>
     </div> <!-- END .wrapper-small -->
 
 	<div class="spacer-big"></div>
