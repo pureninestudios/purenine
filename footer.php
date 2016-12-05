15415326b5413d1cc22181bbd0e9579e6485ed59
Thomas Klinger <tklinger@pure9studios.com>
2016-12-04 20:38:26 -0500

    Php

---
+++ footer.php
@@ -0,0 +1,46 @@
+	<!-- Footer -->
+    <footer class="alternate hidden">
+		<div id="footer-container">
+            <div class="outer">        
+				<div class="inner"> 
+					
+		<p class="copyright">2016 © PURENINE Studios Inc.</p>
+		<div class="scrolltotop hidden"><span class="holder"></span></div>
+		<div class="icon-scroll-line"></div>
+
+
+		<ul class="socials">
+		<li><a href="https://twitter.com/PURENINE" target="_blank"><i class="fa fa-twitter"></i></a></li>
+		<li><a href="https://www.facebook.com/pureninestudios" target="_blank"><i class="fa fa-facebook"></i></a></li>
+		<li><a href="https://www.instagram.com/purenine/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+		<li><a href="https://www.linkedin.com/in/thomasklinger" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+		<li><a href="https://dribbble.com/purenine" target="_blank"><i class="fa fa-dribbble"></i></a></li></ul>	
+			</div></div>
+		</div>	
+	</footer>
+	<!--/Footer -->
+</div>    
+<!--/Page Content -->
+
+<script type='text/javascript' src='../wp-content/plugins/contact-form-7/includes/js/jquery.form.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _wpcf7 = {"loaderUrl":"http:\/\/clapat.ro\/themes\/soho-wordpress\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif","recaptcha":{"messages":{"empty":"Please verify that you are not a robot."}},"sending":"Sending ..."};
+/* ]]> */
+</script>
+<script type='text/javascript' src='../wp-content/plugins/contact-form-7/includes/js/scripts.js'></script>
+<script type='text/javascript' src='../wp-content/themes/soho/js/plugins.js'></script>
+<script type='text/javascript' src='../wp-includes/js/comment-reply.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var ClapatSohoThemeOptions = {"enable_preloader":""};
+var ClapatSecondaryMenuOptions = {"filters_open_caption":"Filters","filters_close_caption":"Close","share_open_caption":"Share","share_close_caption":"Close","search_open_caption":"Search","search_close_caption":"Close"};
+var FullScreenSliderOptions = {"slider_direction":"horizontal","slider_speed":"700","slider_loop_top":"1","slider_loop_bottom":"1"};
+var ClapatMapOptions = {"map_marker_image":"http:\/\/clapat.ro\/themes\/soho-wordpress\/wp-content\/themes\/soho\/images\/marker.png","map_address":"43.270441,6.640888","map_zoom":"16","marker_title":"soho","marker_text":"Here we are. Come to drink a coffee!","map_type":"0","map_api_key":""};
+/* ]]> */
+</script>
+<script type='text/javascript' src='../wp-content/themes/soho/js/scripts.js'></script>
+<script type='text/javascript' src='../wp-includes/js/wp-embed.min.js'></script>
+<script type='text/javascript' src='../wp-content/plugins/js_composer/assets/js/dist/js_composer_front.min.js'></script>
+</body>
+</html>
