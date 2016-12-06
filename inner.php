326d286678ec3ed292d49d0de0d23515e20a58f5
Dean Elliott <dean@deanelliott.me>
2016-12-06 11:51:27 +0000

    Inner page layout

--- inner.php
+++ inner.php
@@ -1,53 +1,62 @@
 <?php include('header.php'); ?>
-
-	 <!-- Page Content -->
-	<div id="page-content">
-
+		<!-- Page Content -->
+		<div id="page-content">
 
 
+		<!-- Hero -->
+        <div id="hero" class="hidden hero-large">
+			<div id="hero-styles" class=" parallax-hero opacity-hero">
 
+			<div class="hero-image" style="">
+            	<div class="overlay" style="background-color:rgba(255, 255, 255, 1.0)">
+                    <!-- Slide Caption -->
+                    <div class="clapat-caption animated ">
+                        <div class="caption-content center-center">
+                            <h1 class="big-title">Madden Live <br />Challenge</h1>
+							<p class="smaller">Conceptualized ideas and designed a website <br />for the Madden Challenge Tournament.</p>
 
-        <!-- Main -->
-        <div id="main">
-		<div id="main-content" class="hidden">
-						<div class="vc_row wpb_row vc_row-fluid text-align-center vc_custom_1460511306389"><div class="container"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner "><div class="wpb_wrapper"><h1 class=" big-title">black book</h1></div></div></div></div></div><div class="vc_row wpb_row vc_row-fluid full light-content vc_custom_1460511677817 vc_row-has-fill"><div class="container"><div class="wpb_column vc_column_container vc_col-sm-6 vc_col-has-fill"><div class="vc_column-inner vc_custom_1460511377651"><div class="wpb_wrapper">
-	<div class="wpb_single_image wpb_content_element vc_align_left">
+							<ul class="smaller hero-info">
+								<li>
+									Client: <span>EA Sports / Twitch</span>
+								</li>
 
-		<figure class="wpb_wrapper vc_figure">
-			<div class="vc_single_image-wrapper   vc_box_border_grey"><img width="1000" height="750" src="../../wp-content/uploads/2016/04/1-7.jpg" class="vc_single_image-img attachment-full" alt="1" srcset="../../wp-content/uploads/2016/04/1-7.jpg 1000w, ../../wp-content/uploads/2016/04/1-7-300x225.jpg 300w, ../../wp-content/uploads/2016/04/1-7-768x576.jpg 768w" sizes="(max-width: 1000px) 100vw, 1000px" /></div>
-		</figure>
-	</div>
-</div></div></div><div class="wpb_column vc_column_container vc_col-sm-6 vc_col-has-fill"><div class="vc_column-inner vc_custom_1460511847731"><div class="wpb_wrapper">
-	<div class="wpb_single_image wpb_content_element vc_align_left">
+								<li>
+									Agency: <span>GoodGame</span>
+								</li>
 
-		<figure class="wpb_wrapper vc_figure">
-			<div class="vc_single_image-wrapper   vc_box_border_grey"><img width="1000" height="750" src="../../wp-content/uploads/2016/04/2-5.jpg" class="vc_single_image-img attachment-full" alt="2" srcset="../../wp-content/uploads/2016/04/2-5.jpg 1000w, ../../wp-content/uploads/2016/04/2-5-300x225.jpg 300w, ../../wp-content/uploads/2016/04/2-5-768x576.jpg 768w" sizes="(max-width: 1000px) 100vw, 1000px" /></div>
-		</figure>
-	</div>
-</div></div></div></div></div><div class="vc_row wpb_row vc_row-fluid full vc_custom_1460511457424 vc_row-has-fill"><div class="container"><div class="wpb_column vc_column_container vc_col-sm-6 vc_col-has-fill"><div class="vc_column-inner vc_custom_1460511377651"><div class="wpb_wrapper">
-	<div class="wpb_single_image wpb_content_element vc_align_left">
+								<li>
+									Services: <span>Strategy, UX, UI, Visual Design</span>
+								</li>
+							</ul>
+						</div>
+                    </div>
+                    <!--/Slide Caption -->
+                </div>
+            </div>
+						<div class="hero-scroll-icons">
+            	<div class="icon-scroll"></div>
+            	<div class="icon-scroll-line"></div>
+            </div>
+						</div>
+		</div>
+        <!--/Hero -->
 
-		<figure class="wpb_wrapper vc_figure">
-			<div class="vc_single_image-wrapper   vc_box_border_grey"><img width="1000" height="750" src="../../wp-content/uploads/2016/04/4-5.jpg" class="vc_single_image-img attachment-full" alt="4" srcset="../../wp-content/uploads/2016/04/4-5.jpg 1000w, ../../wp-content/uploads/2016/04/4-5-300x225.jpg 300w, ../../wp-content/uploads/2016/04/4-5-768x576.jpg 768w" sizes="(max-width: 1000px) 100vw, 1000px" /></div>
-		</figure>
-	</div>
-</div></div></div><div class="wpb_column vc_column_container vc_col-sm-6 vc_col-has-fill"><div class="vc_column-inner vc_custom_1460511494193"><div class="wpb_wrapper">
-	<div class="wpb_single_image wpb_content_element vc_align_left">
+					<!-- Main -->
+			<div id="main">
+				<div id="main-content" class="hidden">
 
-		<figure class="wpb_wrapper vc_figure">
-			<div class="vc_single_image-wrapper   vc_box_border_grey"><img width="1000" height="750" src="../../wp-content/uploads/2016/04/3-5.jpg" class="vc_single_image-img attachment-full" alt="3" srcset="../../wp-content/uploads/2016/04/3-5.jpg 1000w, ../../wp-content/uploads/2016/04/3-5-300x225.jpg 300w, ../../wp-content/uploads/2016/04/3-5-768x576.jpg 768w" sizes="(max-width: 1000px) 100vw, 1000px" /></div>
-		</figure>
-	</div>
-</div></div></div></div></div><div class="vc_row wpb_row vc_row-fluid text-align-center vc_custom_1460511306389"><div class="container"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner "><div class="wpb_wrapper"><h2>Four ready to use mokups</h2><div class="vc_empty_space"  style="height: 20px" ><span class="vc_empty_space_inner"></span></div>
-<a class="clapat-button" href="http://themeforest.net/user/clapat/portfolio" target="_blank">Download</a></div></div></div></div></div>
-							<!-- Project Navigation -->
-    <div class="project-nav">
+					<div class="single-project-inner">
 
-        <div class="item revealed dark-content"><a href="../good-food/index.html" class="animation-link"><div style="background-image:url('../../wp-content/uploads/2016/04/6_thumb.jpg')" class="item-content"></div><div class="item-overlay"><div class="outer"><div class="inner"><span class="item-cat">Prev Project</span><span class="item-title">Good Food</span></div></div></div></a></div>
-		<div class="item revealed dark-content"><a href="../sexy-woman/index.html" class="animation-link"><div style="background-image:url('../../wp-content/uploads/2016/04/4_thumb.jpg')" class="item-content"></div><div class="item-overlay"><div class="outer"><div class="inner"><span class="item-cat">Next Project</span><span class="item-title">Sexy Woman</span></div></div></div></a></div>
-    </div>
-    <!--/Project Navigation -->		</div>
-        </div>
-        <!--/Main -->
+						<img class="single-project-image" src="imgs/madden-image.jpg" alt="Madden Live" />
+
+						<img class="single-project-image" src="imgs/madden-image.jpg" alt="Madden Live" />
+
+					</div>
+
+					<div class="vc_row wpb_row vc_row-fluid text-align-center vc_custom_1460234445834"><div class="container"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner "><div class="wpb_wrapper"><h2>Available for new projects<br> & freelance gigs.</h2><div class="vc_empty_space"  style="height: 20px" ><span class="vc_empty_space_inner"></span></div>
+<a class="clapat-button animation-link" href="../contact/index.html" target="_self">Drop us a line</a></div></div></div></div></div>
+				</div>
+			</div>
+			<!--/Main -->
 
 <?php include('footer.php'); ?>
