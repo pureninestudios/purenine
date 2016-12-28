5245f1beaa741a7a3558eb4ca002675a8352a11c
Thomas Klinger <tklinger@pure9studios.com>
2016-12-28 09:46:42 -0500

    Audiomack page

---
+++ audiomack.php
@@ -0,0 +1,67 @@
+<?php include('header.php'); ?>
+
+	<div class="scroll-line-track"><span class="scroll-line"></span></div>
+		<!-- Page Content -->
+		<div id="page-content">
+
+
+		<!-- Hero -->
+        <div id="hero" class="hidden hero-large">
+			<div id="hero-styles" class=" parallax-hero opacity-hero">
+
+			<div class="hero-image" style="">
+            	<div class="overlay" style="background-color:rgba(255, 255, 255, 1.0)">
+                    <!-- Slide Caption -->
+                    <div class="clapat-caption animated ">
+                        <div class="caption-content center-center">
+                            <h1 class="big-title">Audiomack</h1>
+							<p style="padding-bottom: 10px;">Created the new Audiomack.com, as well as the iOS and Android apps.</p>
+
+							<ul class="hero-info">
+								<li>
+									Client: <span>Audiomack</span>
+								</li>
+
+								<li>
+									Services: <span>Visual Design, UX, UI, Apps</span>
+								</li>
+							</ul>
+						</div>
+                    </div>
+                    <!--/Slide Caption -->
+                </div>
+            </div>
+						<div class="hero-scroll-icons">
+            	<div class="icon-scroll"></div>
+            	<div class="icon-scroll-line"></div>
+            </div>
+						</div>
+		</div>
+        <!--/Hero -->
+
+					<!-- Main -->
+			<div id="main">
+				<div id="main-content" class="hidden">
+
+					<div class="single-project-inner">
+
+						<img class="single-project-image" src="imgs/portfolio-am-single-mobile.jpg" alt="" />
+
+						<img class="single-project-image" src="imgs/portfolio-am-full-mobile.jpg" alt="" />
+
+						<img class="single-project-image" src="imgs/portfolio-am-1.jpg" alt="" />
+
+						<img class="single-project-image" src="imgs/portfolio-am-2.jpg" alt="" />
+
+						<img class="single-project-image" src="imgs/portfolio-am-3.jpg" alt="" />
+
+						<img class="single-project-image" src="imgs/portfolio-am-4.jpg" alt="" />
+
+					</div>
+
+					
+				</div>
+			</div>
+			<!--/Main -->
+
+<?php include('footer.php'); ?>
