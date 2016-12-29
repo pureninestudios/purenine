ee0e9fd79ddfa32eeae7d2423611eeb78a026d1d
Thomas Klinger <tklinger@pure9studios.com>
2016-12-29 11:59:50 -0500

    Adding GA script tag

--- footer.php
+++ footer.php
@@ -24,7 +24,16 @@
 	<!--/Footer -->
 </div>
 <!--/Page Content -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-423279-8', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <script type='text/javascript' src='../wp-content/plugins/contact-form-7/includes/js/jquery.form.min.js'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
