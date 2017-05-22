161f3a36d4d58a0c61fe370d60dd2f83b64d281a
Thomas Klinger <tklinger@pure9studios.com>
2017-05-22 17:46:02 -0400

    Adding metajive to hero

--- index.php
+++ index.php
@@ -20,11 +20,12 @@
                         </div>
                     </div><!-- .hero-content -->
                     <?php
-                        $heroImages = array(
+                        $heroImages = array(  
                             array('top-image-twitch.jpg', 'twitch.php'),
                             array('top-image-pga.jpg', 'pgatour.php'),
                             array('top-image-natgeo.jpg', 'explorer.php'),
                             array('top-image-epicured.jpg', 'https://www.instagram.com/p/BSM1zBthXDR'),
+                            array('top-image-metajive.jpg', 'https://www.instagram.com/purenine/'),
                             array('top-image-nba.jpg', 'http://www.adweek.com/agencies/ready-set-rocket-is-helping-brands-find-their-place-in-the-digital-space/'),
                         );
                         shuffle($heroImages);
