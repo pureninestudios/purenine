a3f7311e700fc06b8a6c09ad37960073835c37dc
Thomas Klinger <tklinger@pure9studios.com>
2017-06-08 19:08:14 -0400

    copy change to nba

--- index.php
+++ index.php
@@ -81,7 +81,7 @@
                                         <h4>NBA.com</h4>
                                     </a>
                                     <p>
-                                       Worked with the team at Ready Set Rocket and the #NBA on a variety of digital products. Contact us for details.
+                                       Partnered with #readysetrocket to design digital products for the NBA. Contact us for details.
                                     </p>
                                 </article><!-- .post-body end -->.
                                 <div class="post-footer">
